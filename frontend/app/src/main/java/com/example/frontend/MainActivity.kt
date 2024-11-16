02455fd037e7b16565f7a29cef886aece3ed339f
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2024-11-16 11:03:45 +0100

    create todoItem & activityMain viewBinding

--- frontend/app/src/main/java/com/example/frontend/MainActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.frontend
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.frontend.databinding.ActivityMainBinding
+import com.example.frontend.databinding.TodoItemsBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var todoItemBinding : TodoItemsBinding
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        todoItemBinding = TodoItemsBinding.inflate(layoutInflater)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(todoItemBinding.root)
+        setContentView(activityMainBinding.root)
     }
 }
